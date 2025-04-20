66ed75c8e063179bdd0da13b63bd57e688f32ff9
dangha <nguyendangha2405@gmail.com>
2025-04-21 00:16:08 +0700

    Refactor: Moved prediction logic from views.py to models.py

--- skin_disease_predictor/predictor/models.py
+++ skin_disease_predictor/predictor/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+# models.py
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
-# Create your models here.
+class SkinDiseaseModel:
+    def __init__(self):
+        model_path = r'D:\VKU\ky_6\Machine_Learning(2)\output\trained_skin_disease_model.keras'
+        self.model = tf.keras.models.load_model(model_path)
+        self.class_names = [
+            '1. Eczema 1677',
+            '2. Melanoma 15.75k',
+            '3. Atopic Dermatitis - 1.25k',
+            '4. Basal Cell Carcinoma (BCC) 3323',
+            '5. Melanocytic Nevi (NV) - 7970'
+        ]
+
+    def predict(self, image_path):
+        img = load_img(image_path, target_size=(128, 128))
+        input_arr = img_to_array(img)
+        input_arr = np.expand_dims(input_arr, axis=0)
+        predictions = self.model.predict(input_arr)
+        result_index = np.argmax(predictions)
+        return self.class_names[result_index]
